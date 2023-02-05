64bb143dec8c16f397fa4b88e2683ec7b6653233
Jacob Salmon <123907965+Nutmeg43@users.noreply.github.com>
2023-02-04 19:37:50 -0500

    Add files via upload

---
+++ fifo_intf.sv
@@ -0,0 +1,30 @@
+interface fifo_intf#(
+    BITSIZE = 8
+)(
+    input w_clk,
+    input r_clk
+);                        
+    logic w_enable;                               //Enable signal for writing data
+    logic r_enable;                               //Enable signal for reading data
+    logic reset;                                  //Sigal to reset read/write pointers (can not write/read during reset)
+    logic [BITSIZE-1:0] wdata;                    //Wrte data
+    logic [BITSIZE-1:0] rdata;                    //Read data
+    logic full;                                   //Signal stating if FIFO is full
+    logic empty;                                  //Signal stating if FIFO is empty
+
+    clocking w_cb @(posedge w_clk);
+        output w_enable;
+        output wdata;
+        output reset;
+        input  full;        
+    endclocking
+
+
+    clocking r_cb @(posedge r_clk);
+        output r_enable;
+        output reset;
+        input  rdata;
+        input  empty;
+    endclocking
+    
+endinterface
